821b2b743cf35f6f824dc774446a0d7f2d523986
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-20 16:24:00 +0200

    drivers: scsi: ufs: Import Xiaomi changes
    Change-Id: I349847d5a804401d90e6c54bf2e28a39130a91eb

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -228,7 +228,11 @@ static void ufshcd_update_uic_error_cnt(struct ufs_hba *hba, u32 reg, int type)
 /* Query request retries */
 #define QUERY_REQ_RETRIES 3
 /* Query request timeout */
+#ifdef CONFIG_MACH_XIAOMI_SM8250
+#define QUERY_REQ_TIMEOUT 3000 /* 3.0 seconds */
+#else
 #define QUERY_REQ_TIMEOUT 1500 /* 1.5 seconds */
+#endif
 
 /* Task management command timeout */
 #define TM_CMD_TIMEOUT	100 /* msecs */
@@ -469,6 +473,10 @@ static struct ufs_dev_fix ufs_fixups[] = {
 		UFS_DEVICE_QUIRK_PA_HIBER8TIME),
 	UFS_FIX(UFS_VENDOR_SAMSUNG, "KLUDG4UHDB-B2D1",
 		UFS_DEVICE_QUIRK_PA_HIBER8TIME),
+#ifdef CONFIG_MACH_XIAOMI_SM8250
+	UFS_FIX(UFS_VENDOR_SAMSUNG, "KLUFG8RHDA-B2D1",
+		UFS_DEVICE_QUIRK_PA_HIBER8TIME),
+#endif
 	END_FIX
 };
 
@@ -5693,7 +5701,9 @@ static int ufshcd_complete_dev_init(struct ufs_hba *hba)
 	int i = 0;
 	int err;
 	bool flag_res = 1;
+#ifdef CONFIG_MACH_XIAOMI_SM8250
 	ktime_t timeout;
+#endif
 
 	err = ufshcd_query_flag_retry(hba, UPIU_QUERY_OPCODE_SET_FLAG,
 		QUERY_FLAG_IDN_FDEVICEINIT, NULL);
@@ -5704,30 +5714,35 @@ static int ufshcd_complete_dev_init(struct ufs_hba *hba)
 		goto out;
 	}
 
+#ifdef CONFIG_MACH_XIAOMI_SM8250
 	/*
-	 * Some vendor devices are taking longer time to complete its internal
-	 * initialization, so set fDeviceInit flag poll time to 5 secs
-	 */
+	* Some vendor devices are taking longer time to complete its internal
+	* initialization, so set fDeviceInit flag poll time to 5 secs
+	*/
 	timeout = ktime_add_ms(ktime_get(), 5000);
 
 	/* poll for max. 5sec for fDeviceInit flag to clear */
+	i = 0;
 	while (1) {
 		bool timedout = ktime_after(ktime_get(), timeout);
+#endif
 		err = ufshcd_query_flag_retry(hba, UPIU_QUERY_OPCODE_READ_FLAG,
-					QUERY_FLAG_IDN_FDEVICEINIT, &flag_res);
+			QUERY_FLAG_IDN_FDEVICEINIT, &flag_res);
+#ifdef CONFIG_MACH_XIAOMI_SM8250
 		if (err || !flag_res || timedout)
 			break;
 
 		/*
-		 * Poll for this flag in a tight loop for first 1000 iterations.
-		 * This is same as old logic which is working for most of the
-		 * devices, so continue using the same.
-		 */
+		* Poll for this flag in a tight loop for first 1000 iterations.
+		* This is same as old logic which is working for most of the
+		* devices, so continue using the same.
+		*/
 		if (i == 1000)
 			msleep(20);
 		else
 			i++;
 	}
+#endif
 
 	if (err)
 		dev_err(hba->dev,
@@ -6125,6 +6140,39 @@ static int ufshcd_get_lu_wp(struct ufs_hba *hba,
 					  sizeof(*b_lu_write_protect));
 	return ret;
 }
+
+#ifndef CONFIG_MACH_XIAOMI_SM8250
+/*
+ * ufshcd_get_wb_alloc_units - returns "dLUNumWriteBoosterBufferAllocUnits"
+ * @hba: per-adapter instance
+ * @lun: UFS device lun id
+ * @d_lun_wbb_au: pointer to buffer to hold the LU's alloc units info
+ *
+ * Returns 0 in case of success and d_lun_wbb_au would be returned
+ * Returns -ENOTSUPP if reading d_lun_wbb_au is not supported.
+ * Returns -EINVAL in case of invalid parameters passed to this function.
+ */
+static int ufshcd_get_wb_alloc_units(struct ufs_hba *hba,
+			    u8 lun,
+			    u8 *d_lun_wbb_au)
+{
+	int ret;
+
+	if (!d_lun_wbb_au)
+		ret = -EINVAL;
+
+	/* WB can be supported only from LU0..LU7 */
+	else if (lun >= UFS_UPIU_MAX_GENERAL_LUN)
+		ret = -ENOTSUPP;
+	else
+		ret = ufshcd_read_unit_desc_param(hba,
+					  lun,
+					  UNIT_DESC_PARAM_WB_BUF_ALLOC_UNITS,
+					  d_lun_wbb_au,
+					  sizeof(*d_lun_wbb_au));
+	return ret;
+}
+#endif
 
 /**
  * ufshcd_get_lu_power_on_wp_status - get LU's power on write protect
@@ -8507,8 +8555,11 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 	int err;
 	size_t buff_len;
 	u8 model_index, lun;
+#ifdef CONFIG_MACH_XIAOMI_SM8250
 	u8 *desc_buf;
+#else
 	u32 d_lu_wb_buf_alloc;
+#endif
 
 	buff_len = max_t(size_t, hba->desc_size.dev_desc,
 			 QUERY_DESC_MAX_SIZE + 1);
@@ -8536,6 +8587,7 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 	model_index = desc_buf[DEVICE_DESC_PARAM_PRDCT_NAME];
 
 
+#ifndef CONFIG_MACH_XIAOMI_SM8250
 	/* Enable WB only for UFS-3.1 or UFS-2.2 OR if desc len >= 0x59 */
 	if ((dev_desc->wspecversion >= 0x310) ||
 	    (dev_desc->wspecversion == 0x220) ||
@@ -8576,6 +8628,7 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 	}
 
 skip_unit_desc:
+#endif
 	/* Zero-pad entire buffer for string termination. */
 	memset(desc_buf, 0, buff_len);
 
@@ -9210,6 +9263,10 @@ reinit:
 		scsi_scan_host(hba->host);
 		pm_runtime_put_sync(hba->dev);
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_SM8250
+	ufshcd_set_auto_hibern8_timer(hba);
+#endif
 
 out:
 	if (ret) {
