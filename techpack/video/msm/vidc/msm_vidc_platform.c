fa0cbfa837175f3e243b570a561d2da6c03db64e
yarpiin <yarpiin@gmail.com>
2021-12-11 00:54:13 +0000

    Merge tag 'LA.UM.9.12.r1-13500-SMxx50.QSSI12.0' of https://source.codeaurora.org/quic/la/platform/vendor/opensource/video-driver into LA.UM.9.12.r1-XXXX-SMxx50.QSSI12.0

--- techpack/video/msm/vidc/msm_vidc_platform.c
+++ techpack/video/msm/vidc/msm_vidc_platform.c
@@ -991,7 +991,7 @@ static struct msm_vidc_common_data lito_common_data_v0[] = {
 	},
 	{
 		.key = "qcom,decode-batching",
-		.value = 1,
+		.value = 0,
 	},
 	{
 		.key = "qcom,batch-timeout",
@@ -1339,7 +1339,7 @@ static struct msm_vidc_common_data kona_common_data[] = {
 	},
 	{
 		.key = "qcom,decode-batching",
-		.value = 0,
+		.value = 1,
 	},
 	{
 		.key = "qcom,batch-timeout",
