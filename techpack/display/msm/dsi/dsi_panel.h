9c08fa00192e73b6414ea36704a70609ba14bc5b
nullxception <enuma.alrizky@gmail.com>
2022-09-01 23:51:57 +0100

    techpack: display: temporary disable DC dimming when in hbm mode

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -239,6 +239,8 @@ struct dsi_panel {
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
 	int hbm_mode;
+	bool resend_ea_hbm;
+
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
